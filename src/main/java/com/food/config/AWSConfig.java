7f55303c013fac6382c4d5955013bf6b30ca1f3b
tapeshchavle <tapeshchawle@gmail.com>
2025-05-09 02:31:42 +0530

    updated

--- src/main/java/com/food/config/AWSConfig.java
+++ src/main/java/com/food/config/AWSConfig.java
@@ -12,7 +12,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 //this class become configuration class for s3 
 public class AWSConfig {
-	@Value("${${AWS_ACCESS_KEY}}")
+	@Value("${AWS_ACCESS_KEY}")
 	private String accessKey;
 	@Value("${AWS_SECRET_KEY }")
 	private String secretKey;
