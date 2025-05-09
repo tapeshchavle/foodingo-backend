0f24ecfb5bf8df8f92bdeda1d98d84ef363d4bc9
tapeshchavle <tapeshchawle@gmail.com>
2025-05-09 16:20:52 +0530

    new jar added

--- src/main/java/com/food/config/SecurityConfig.java
+++ src/main/java/com/food/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
 	}
 	private UrlBasedCorsConfigurationSource corsConfigurationSource(){
 		CorsConfiguration config=new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174","https://foodingo.netlify.app"));
+		config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174","https://foodingo.netlify.app","https://foodingo-admin.netlify.app"));
 		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
 		config.setAllowedHeaders(List.of("Authorization","Content-Type"));
 		config.setAllowCredentials(true);
